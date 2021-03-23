929a5f63d0e688d27e2084f02e499308db04bc85
matias <matu.93tkd@gmail.com>
2021-03-23 15:06:53 -0300

    Initial commit

---
+++ join.sql
@@ -0,0 +1,15 @@
+--select * from Employees
+--	where FirstName like 'a%' and City = 'London'
+
+--select p.*, c.CategoryName, s.CompanyName from Products as p
+--	join Categories as c on p.CategoryID = c.CategoryID
+--	join Suppliers as s on p.SupplierID = s.SupplierID
+--	where s.CompanyName like 'p%' and p.UnitPrice = 7.75
+
+select * from Orders as o
+	--join Customers as c on o.CustomerID = c.CustomerID
+	--join Employees as e on o.EmployeeID = e.EmployeeID
+	where o.ShipName between 'Folk och fä HB' and 'Hanari Carnes'
+	
+	--hola mundo!
+	
